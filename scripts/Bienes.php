34eb0b541f4ec7c85a405c287d6fb8ccda8bbce3
Jonathan Cruz <jonathan.cruz89@gmail.com>
2020-10-27 19:00:42 -0500

    se crea entorno con docker

--- scripts/Bienes.php
+++ scripts/Bienes.php
@@ -2,10 +2,10 @@
 require_once __DIR__ . '/Database.php';
 
 define('DB_TYPE', 'mysql');
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', 'mysql');
 define('DB_NAME', 'intelcost');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', '1234');
 
 class Bienes {
 	public $db;
